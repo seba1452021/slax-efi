4b25fd7c87b369f87baf64cf3fbe477450ebf071
seba1452021 <92406968+seba1452021@users.noreply.github.com>
2021-12-04 02:03:55 -0300

    Update slax-efi.sh

--- slax-efi.sh
+++ slax-efi.sh
@@ -2,23 +2,29 @@
 thumbdrive=/dev/sda
 d=`mktemp -d`
 cd $d
-wget https://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso
-test -f slax-64bit-*.iso
-apt install -y parted syslinux-common syslinux-efi
+wget https://ftp.sh.cvut.cz/slax/Slax-9.x/slax-64bit-9.11.0.iso -o slax-64bit.iso
+wget https://download.plop.at/plopkexec/plopkexec-1.6-bin.tar.gz -o plopkexec.tar.gz
+wget https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.gz -o syslinux.tar.gz
+apt install -y parted syslinux-common syslinux-efi p7zip-full unp
 dd if=/dev/zero of=${thumbdrive} bs=1M count=5
 sync
 /sbin/parted ${thumbdrive} mklabel msdos --script
 /sbin/parted ${thumbdrive} mkpart primary 0% 100% --script
 mkfs.vfat ${thumbdrive}1
-mkdir /media/{source,target}
-mount -o loop -t iso9660 slax-64bit-*.iso /media/source
+mkdir /media/target
+7z x slax-64bit.iso
+unp plopkexec.tar.gz syslinux.tar.gz
 mount -t vfat ${thumbdrive}1 /media/target
-cp -axv /media/source/slax /media/target/slax
-bash -x /media/target/slax/boot/bootinst.sh
+mv slax /media/target/
 mkdir -p /media/target/EFI/Boot/
+mv plopkexec/plopkexec64 /media/target/EFI/Boot/
+mv syslinux/efi64/com32/elflink/ldlinux/ldlinux.e64 /media/target/EFI/Boot/
+mv syslinux/efi64/com32/lib/libcom32.c32 /media/target/EFI/Boot/
+mv syslinux/efi64/com32/libutil/libutil.c32 /media/target/EFI/Boot/
+mv syslinux/efi64/com32/menu/{menu.c32,vesamenu.c32} /media/target/EFI/Boot/
+mv syslinux/efi64/com32/modules/{poweroff.c32,reboot.c32} /media/target/EFI/Boot/
 cp /usr/lib/SYSLINUX.EFI/efi64/syslinux.efi /media/target/EFI/Boot/bootx64.efi
-cd /usr/lib/syslinux/modules/efi64/
-cp ldlinux.e64 menu.c32 libcom32.c32 libutil.c32 vesamenu.c32 /media/target/EFI/Boot
+bash -x /media/target/slax/boot/bootinst.sh
 cat > /media/target/EFI/Boot/syslinux.cfg <<"SYSLINUXCFG"
 PROMPT 0
 TIMEOUT 40
@@ -63,4 +69,17 @@ LABEL toram
 MENU LABEL Run Slax (Copy to RAM)
 KERNEL /slax/boot/vmlinuz
 APPEND vga=normal initrd=/slax/boot/initrfs.img load_ramdisk=1 prompt_ramdisk=0 rw printk.time=0 consoleblank=0 slax.flags=toram
+
+LABEL poweroff
+MENU LABEL Power Off
+COM32 poweroff.c32
+
+LABEL reboot
+MENU LABEL Reboot
+COM32 reboot.c32
+
+LABEL plopkexec
+kernel plopkexec64
+append quiet
+
 SYSLINUXCFG
