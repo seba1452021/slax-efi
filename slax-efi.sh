44638c8db302f8b527b9c105478add6a15b3037c
seba1452021 <92406968+seba1452021@users.noreply.github.com>
2022-08-07 18:55:28 -0300

    Add files via upload

--- slax-efi.sh
+++ slax-efi.sh
@@ -7,21 +7,22 @@ cd $dir
 directory=$(pwd)
 
 yes n | rm -r -f -i !(${0}|autorun.inf|logo.ico) &>/dev/null
-case $? in 1) ;; 0) echo -e "existen cosas aparte de este script. \n \n ¿desea superponer? S/N \n \n" && read -n 2 resp KEY && sleep 2 && clear ;; esac
-case $resp in "s"|"S"|"y"|"Y") boot=syslinux ;; *) boot=BOOT ;; esac
+case $? in 0) echo -e "existen cosas aparte de este script. \n \n ¿desea superponer? S/N \n \n" && read -n 2 resp KEY && sleep 2 && clear ;; esac
+case $resp in [Yy]*|[Ss]*) boot=syslinux ;; *) boot=BOOT ;; esac
 
 echo -e "\n cargando.. \n"
 
-PART="$(df . | tail -n 1 | tr -s " " | cut -d " " -f 1)"
-DEV="$(echo "$PART" | sed -r "s:[0-9]+\$::" | sed -r "s:([0-9])[a-z]+\$:\\1:i")"   #"
-NUM="$(echo $PART | sed -e "s|"$DEV"||g" -e "s|"[a-z]"||g")"
-VARS=`blkid ${PART} | sed -e "s|${PART}:||g" | sed -e 's|"||g'`
+PART=$(df . | tail -n 1 | tr -s " " | cut -d " " -f 1)
+DEV=$(echo $PART | sed -r "s:[0-9]+\$::" | sed -r "s:([0-9])[a-z]+\$:\\1:i")
+NUM=$(echo $PART | sed -e "s|$DEV||g" -e "s|[a-z]||g")
+
+VARS=`blkid ${PART} | sed -e "s|${PART}: ||g" | sed -e 's|"||g'`
 export ${VARS}
 
 clear
 echo -e "\n opteniendo y/o actualizando dependencias.. \n"
 
-apt install -y -f parted p7zip{,-full} wget efibootmgr syslinux-common &>/dev/null
+apt install -y -f parted p7zip{,-full} wget efibootmgr mtools &>/dev/null
 
 clear
 echo -e "\n descargando y comprobando archivos.. \n"
@@ -29,9 +30,11 @@ echo -e "\n descargando y comprobando archivos.. \n"
 i=1
 until [ $i -eq 0 ]
 do
-if [ -f slax-64bit-11.3.0.iso ]; then rm slax-64bit-11.3.0.iso; fi
-test -f slax-64bit-11.3.0.iso || wget https://ftp.sh.cvut.cz/slax/Slax-11.x/slax-64bit-11.3.0.iso &>/dev/null
-test -f md5.txt || echo '9d94c1796ba4c79fb05bb9f35c3fe188  slax-64bit-11.3.0.iso' > md5.txt
+test -f md5.txt || wget https://ftp.sh.cvut.cz/slax/Slax-11.x/md5.txt &>/dev/null
+grep 64bit md5.txt | tail -n 1 | cat > md5.txt.1 && mv md5.txt.1 ./md5.txt
+imagen=$(cut -d " " -f 3 md5.txt)
+test -f ${imagen} || rm ${imagen} &>/dev/null
+test -f ${imagen} || wget https://ftp.sh.cvut.cz/slax/Slax-11.x/${imagen} &>/dev/null
 md5sum -c md5.txt &>/dev/null
 i=$?
 done
@@ -39,7 +42,7 @@ done
 clear
 echo -e "\n descomprimiendo archivos.. \n"
 
-7z x slax-64bit-11.3.0.iso &>/dev/null
+7z x ${imagen} &>/dev/null
 
 clear
 echo -e "\n procesando .. \n"
@@ -48,7 +51,7 @@ mkdir boot -p EFI/${boot}
 
 mv slax/boot/EFI/Boot/!(bootx64.efi) EFI/${boot}/
 mv slax/boot/{help.txt,initrfs.img,vmlinuz,*.png} boot/
-rm readme* slax-64bit-11.3.0.iso md5.txt -r slax/boot '[BOOT]'
+rm readme* ${imagen} md5.txt -r slax/boot '[BOOT]'
 
 boot_file="BOOTx64.EFI"
 
@@ -82,16 +85,24 @@ sed -i "s|vesamenu.c32|/EFI/${boot}/vesamenu.c32\nMENU TITLE Boot (EFI)|g" sysli
 echo "${boot_file},${boot},,This is the boot entry for syslinux" | tee -a BOOTX64.CSV &>/dev/null
 
 if [ ${TYPE} == "vfat" ]; then
+
 parted -s ${DEV} set ${NUM} boot on
 cd $directory
 
 if [ ! -d EFI/BOOT/ ]; then mkdir EFI/BOOT/; fi
 
-file='' && for x in {fallback,fbx64}.efi; do [ "$file" != "" ]; case $? in 1) if [ -f EFI/BOOT/${x} ]; then file="EFI/BOOT/${x}"; fi ;; *) ;; esac; done
+file='' && for x in {fallback,fbx64}.efi; do [ "$file" != "" ]; case $? in 1) if [ -f EFI/BOOT/${x} ]; then file="EFI/BOOT/${x}"; fi ;; esac; done
 
-if [ "$file" = "" ]; then cp /usr/lib/shim/fbx64.efi EFI/BOOT/; fi
+if [ "$file" = "" ]; then file=/usr/lib/shim/fbx64.efi; fi
+if [ ! -e $file ]; then apt-get download shim-signed &>/dev/null && d=`mktemp -d` && dpkg-deb --extract shim-signed*.deb $d &>/dev/null; fi
+cp ${d}/usr/lib/shim/fbx64.efi EFI/BOOT/
 
 if [ -d EFI/syslinux/ ]; then efibootmgr --verbose --disk ${DEV} --part ${NUM} --create --label "Syslinux" --loader /EFI/${boot}/${boot_file} &>/dev/null; fi
+
+bootnum=$(efibootmgr | grep Syslinux | sed -e "s/Syslinux//g" -e 's|Boot||g')
+active=$(echo $bootnum | grep '*' &>/dev/null && echo $?) 
+case $active in 1) efibootmgr --bootnum $bootnum --active ;; esac
+
 fi
 
 clear
